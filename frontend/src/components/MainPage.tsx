399aa74ab4957a17c7ca41c77119e686b2622c1b
Cloud956 <pmlewandowski@gmail.com>
2023-07-11 15:01:26 +0200

    please

--- frontend/src/components/MainPage.tsx
+++ frontend/src/components/MainPage.tsx
@@ -4,7 +4,7 @@ import LeftPanel from "./Layout/LeftPanel/LeftPanel";
 import RightPanel from "./Layout/RightSide/RightPanel";
 import bibiSource from "../images/bibi.jpg";
 const MainPage = () => {
-  const backendIP = "http://0.0.0.0:8000/transformations/";
+  const backendIP = "http://localhost:8000/transformations/";
   const [firstParam, setFirstParam] = useState(0);
   const [secondParam, setSecondParam] = useState(0);
   const [thirdParam, setThirdParam] = useState(0);
